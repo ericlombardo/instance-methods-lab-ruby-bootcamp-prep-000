05bc71e9582e4ff0e6e895cb2ffefaa65963f566
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-19 13:14:34 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+# define Dog class
+class Dog
+# define #bark that puts "Woof!"
+  def bark
+    puts "Woof!"
+  end
+# define #sit => puts "The Dog is sitting"
+  def sit
+    puts "The Dog is sitting"
+  end
+end
