f7a116ecfc41841c3761c62a3e2724a165af5947
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-19 13:16:57 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+# define Person class
+class Person
+# define #walk => puts "The Person is walking"
+  def walk
+    puts "The Person is walking"
+  end
+# define #talk => puts "Hello World!"
+  def talk
+    puts "Hello World!"
+  end
+end
